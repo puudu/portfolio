9736ed5381a46bcd8de6425489e9458e22b55cfa
puudu <104123662+puudu@users.noreply.github.com>
2024-02-05 21:31:32 -0300

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [process.env.NEXT_PUBLIC_BACKEND_HOST_NAME],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_BACKEND_HOST_NAME,
-        pathname: "**",
-      },
-    ],
-  },
+  // images: {
+  //   domains: [process.env.NEXT_PUBLIC_BACKEND_HOST_NAME],
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: process.env.NEXT_PUBLIC_BACKEND_HOST_NAME,
+  //       pathname: "**",
+  //     },
+  //   ],
+  // },
 };
 
 module.exports = nextConfig;
